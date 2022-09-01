fc5779a5833f58026358a4b102eb9716f1ac48bf
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-01 17:25:46 -0300

    read more button added

--- src/components/Tour.tsx
+++ src/components/Tour.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 interface TourProps{
   id: string,
   image: string, 
@@ -7,13 +9,18 @@ interface TourProps{
 }
 
 export function Tour({id, image, info, price, name}: TourProps){
+  const [readMore, setReadMore] = useState(false)
   return(
     <div className="single-tour">
       <img src={image} alt={name}/>
       <div className="tour-info">
         <h4>{name}</h4>
         <h4 className="tour-price">${price}</h4>
-        <p>{info}</p>
+        <p>{readMore ? info : `${info.substring(0,200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? `show less` : `read more`}
+          </button>
+        </p>
         <button className="delete-btn">not interested</button>
       </div>
     </div>
--- src/components/Tours.tsx
+++ src/components/Tours.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Tour } from "./Tour"
 
 interface ToursProps{
