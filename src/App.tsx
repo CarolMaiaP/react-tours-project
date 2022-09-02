0704e4b3b901cd19a05dc9d7edb1241620c7bcb2
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-09-02 11:39:17 -0300

    Loading component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { Loading } from './components/Loading';
 import { Tour } from './components/Tour';
 import { Tours } from './components/Tours'
 
 const api = 'https://course-api.com/react-tours-project';
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
   async function fetchTours(){
+    setLoading(true)
+
     try{
       const response = await fetch(api);
       const tours = await response.json();
       console.log(tours)
+      setLoading(false)
       setTours(tours);
     }catch{
-      alert("erro ao buscar")
+      setLoading(false)
+      console.log(Error)
     }
   }
 
   useEffect(() => {
     fetchTours()
   }, [])
+
+  if(loading){
+    return(
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
 
   return (
     <div className="Tours">
